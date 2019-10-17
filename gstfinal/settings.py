0656dd02619ba9ef543c9043269229b7b0b5c4b2
Poorna Chand <poornachandvanga091099@gmail.com>
2019-10-17 22:24:41 +0530

    Added Django heroku

--- gstfinal/settings.py
+++ gstfinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'nk@u=1-%%#9+5(qiucq3)5!u#(51mkdajk23(-kt+w%g$)&$xc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['onlinegst.herokuapp.com']
 
 
 # Application definition
@@ -128,3 +129,5 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'insert_page'
+
+django_heroku.settings(locals())
